aa489abea69f64fa0a1feed1631d7a56a803b89b
gofar09 <gordon.d.farrell@gmail.com>
2018-07-03 22:35:20 -0400

    Update prices controller for 3pAPI -Fully functional on api side

--- app/controllers/prices_controller.rb
+++ app/controllers/prices_controller.rb
@@ -1,12 +1,13 @@
-class PricesController < ApplicationController
+class PricesController < ProtectedController
   before_action :set_price, only: [:index, :show, :update, :destroy]
 
   # GET /prices
   def index
+    # binding.pry
     @prices = HTTParty.get('https://www.alphavantage.co/query?',
       :headers => { 'Content-Type' => 'application/json' },
       query: { 'function' => 'BATCH_STOCK_QUOTES', 'symbols' => params['price']['symbol'],
-        'apikey' => 'LJVNN3W1ZUYE40G' })
+      'apikey' => 'LJVNN3W1ZUYE40G' })
     render json: @prices
   end
 
@@ -19,15 +20,14 @@ class PricesController < ApplicationController
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_price
-      puts "!!!!!!!!!!!!!!!"
-      puts params['price']['symbol']
+  def set_price
+
       # @p = Price.find(params[:symbol])
       # puts "Symbol is #{@symbol}"
-    end
+  end
 
     # Only allow a trusted parameter "white list" through.
-    def price_params
-      params.require(:symbol).permit(:price)
+  def price_params
+    params.require(:symbol).permit(:price)
     end
 end
